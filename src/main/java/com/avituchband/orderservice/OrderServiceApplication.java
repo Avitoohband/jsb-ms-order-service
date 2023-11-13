efdf2ed6289d2139d652f72531968c4852b2f296
Avi Tuchband <avitoohband@gmail.com>
2023-11-13 18:27:45 +0200

    first commit

---
+++ src/main/java/com/avituchband/orderservice/OrderServiceApplication.java
@@ -0,0 +1,15 @@
+package com.avituchband.orderservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderServiceApplication.class, args);
+
+		//47:51
+	}
+
+}
