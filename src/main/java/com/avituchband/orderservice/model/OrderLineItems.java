b0c9d97e91aec91c3623e5c32591c7b8e0b3f63a
Avi Tuchband <avitoohband@gmail.com>
2023-11-14 13:08:15 +0200

    build models

---
+++ src/main/java/com/avituchband/orderservice/model/OrderLineItems.java
@@ -0,0 +1,18 @@
+package com.avituchband.orderservice.model;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name ="t_order_line_items")
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class OrderLineItems {
+
+}
